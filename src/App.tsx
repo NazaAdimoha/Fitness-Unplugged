718a092c923adb1a0dd7bd33e68054d7306c9d05
nazaadimoha <nazaadimoha98@gmail.com>
2023-10-02 13:06:34 +0100

    push files to repo

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import Navbar from "@/scenes/navbar";
+import Home from "@/scenes/home";
+import OurClasses from "@/scenes/ourClasses";
+import Benefits from "@/scenes/benefits";
+import ContactUs from "@/scenes/contactUs";
+import Footer from "@/scenes/footer";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "@/shared/types";
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app bg-gray-20">
+      <p>Hello World</p>
+    </div>
+  );
+}
+
+
+
+export default App;
