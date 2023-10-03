1acbd38e89e1f2dc33e6e45d26baa5ae8c643a96
nazaadimoha <nazaadimoha98@gmail.com>
2023-10-03 02:22:48 +0100

    set up the nav bar skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import Navbar from "@/scenes/navbar";
-import Home from "@/scenes/home";
-import OurClasses from "@/scenes/ourClasses";
-import Benefits from "@/scenes/benefits";
-import ContactUs from "@/scenes/contactUs";
-import Footer from "@/scenes/footer";
+// import Home from "@/scenes/home";
+// import OurClasses from "@/scenes/ourClasses";
+// import Benefits from "@/scenes/benefits";
+// import ContactUs from "@/scenes/contactUs";
+// import Footer from "@/scenes/footer";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 
@@ -12,7 +12,12 @@ import React from 'react';
 function App() {
   return (
     <div className="app bg-gray-20">
-      <p>Hello World</p>
+      <Navbar />
+      {/* <Home /> */}
+      {/* <OurClasses /> */}
+      {/* <Benefits /> */}
+      {/* <ContactUs /> */}
+      {/* <Footer /> */}
     </div>
   );
 }
