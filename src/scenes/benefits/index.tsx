3dcca0e0eae9c7d90dd70e19aa2519bdd005b998
nazaadimoha <nazaadimoha98@gmail.com>
2023-10-04 14:15:04 +0100

    animated the hamburger icon for the navbar

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -20,10 +20,21 @@ const Benefits = ({
             className="flex flex-col items-center justify-center"
             onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
         >
-            <div>
+            {/* MAIN HEADER */}
+            <div className="md:my-8 md:w-3/5">
                 <HText>
                     So Much More Than Just A Gym
                 </HText>
+                <p className="my-5 text-sm">
+                    We are a community of like-minded people who are all here to give you the support you need to achieve your goals. We are here to help you every step of the way in achieving that Set Body.
+                </p>
+            </div>
+
+            {/* BENEFITS */}
+            <div 
+                className="md:flex items-center justify-center gap-8 md:gap-16 md:mt-16"
+            >
+
             </div>
         </motion.div>
     </section>
--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -5,112 +5,132 @@ import Link from "./Link";
 import useMediaQuery from "@/hooks/useMediaQuery";
 import ActionButton from "@/shared/ActionButton";
 import { SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
 
 type Props = {
-    isTopOfPage: boolean,
-    selectedPage: SelectedPage,
-    setSelectedPage: (page: SelectedPage) => void
-}
+  isTopOfPage: boolean;
+  selectedPage: SelectedPage;
+  setSelectedPage: (page: SelectedPage) => void;
+};
 
-const Navbar = ({
-    isTopOfPage,
-    selectedPage,
-    setSelectedPage
-}: Props) => {
-    const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
-    const flexBetween = "flex items-center justify-between";
-    const isAboveMediumScreen = useMediaQuery("(min-width: 1060px)");
-    const navBarBackground = isTopOfPage ? "bg-transparent" : "bg-primary-100 bg-opacity-90 drop-shadow-xl";
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+  const flexBetween = "flex items-center justify-between";
+  const isAboveMediumScreen = useMediaQuery("(min-width: 1060px)");
+  const navBarBackground = isTopOfPage
+    ? "bg-transparent"
+    : "bg-primary-100 bg-opacity-90 drop-shadow-xl";
+
+  //Animatate hambuger icon state
+  const variants = {
+    open: { opacity: 1, y: 0 },
+    closed: { opacity: 0, y: "-100%" },
+  };
   return (
     <nav>
-        <div 
-            className={`${navBarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}
-        >
-            <div className={`${flexBetween} mx-auto w-5/6`}>
-                <div className={`${flexBetween} w-full gap-16`}>
-                    {/* LEFT SIDE */}
-                    <img alt="logo" src={Logo} />
+      <div
+        className={`${navBarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}
+      >
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div className={`${flexBetween} w-full gap-16`}>
+            {/* LEFT SIDE */}
+            <img alt="logo" src={Logo} />
 
-                    {/* RIGHT SIDE */}
-                    {
-                        isAboveMediumScreen ? (
-                            <div className={`${flexBetween} w-full`}>
-                                <div className={`${flexBetween} gap-8 text-sm`}>
-                                    <Link 
-                                        page="Home"
-                                        selectedPage={selectedPage}
-                                        setSelectedPage={setSelectedPage}
-                                    />
-                                    <Link 
-                                        page="Benefits"
-                                        selectedPage={selectedPage}
-                                        setSelectedPage={setSelectedPage}
-                                    />
-                                    <Link 
-                                        page="Our Classes"
-                                        selectedPage={selectedPage}
-                                        setSelectedPage={setSelectedPage}
-                                    />
-                                    <Link 
-                                        page="Contact us"
-                                        selectedPage={selectedPage}
-                                        setSelectedPage={setSelectedPage}
-                                    />
-                                </div>
-
-                                <div className={`${flexBetween} gap-8`}>
-                                    <p>Sign In</p>
-                                    <ActionButton setSelectedPage={setSelectedPage}>Become a Member</ActionButton>
-                                </div>
-                            </div>
-                        ) : (
-                            <button className="rounded-full bg-secondary-500 p-2" onClick={() => setIsMenuToggled(!isMenuToggled)}>
-                                <Bars3BottomRightIcon className="h-6 w-6 text-white" />
-                            </button>
-                        )
-                    }
+            {/* RIGHT SIDE */}
+            {isAboveMediumScreen ? (
+              <div className={`${flexBetween} w-full`}>
+                <div className={`${flexBetween} gap-8 text-sm`}>
+                  <Link
+                    page="Home"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Benefits"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Our Classes"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Contact us"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
                 </div>
-            </div>
+
+                <div className={`${flexBetween} gap-8`}>
+                  <p>Sign In</p>
+                  <ActionButton setSelectedPage={setSelectedPage}>
+                    Become a Member
+                  </ActionButton>
+                </div>
+              </div>
+            ) : (
+              <button
+                className="rounded-full bg-secondary-500 p-2"
+                onClick={() => setIsMenuToggled(!isMenuToggled)}
+              >
+                <Bars3BottomRightIcon className="h-6 w-6 text-white" />
+              </button>
+            )}
+          </div>
         </div>
+      </div>
 
-        {/* MOBILE MENU */}
-        {
-            isMenuToggled && !isAboveMediumScreen && (
-                <div className="fixed bottom-0 right-0 z-40 w-[300px] h-full bg-primary-100 bg-opacity-90 drop-shadow-xl">
-                    <div className="flex justify-end p-12">
-                        <button className="rounded-full bg-secondary-500 p-2" onClick={() => setIsMenuToggled(!isMenuToggled)}>
-                            <XCircleIcon className="h-6 w-6 text-gray-400" />
-                        </button>
-                    </div>
+      {/* MOBILE MENU */}
+      {isMenuToggled && !isAboveMediumScreen && (
+        <motion.div
+          initial="closed"
+          animate={isMenuToggled ? "open" : "closed"}
+          variants={variants}
+          transition={{ duration: 1 }}
+          className="fixed bottom-0 right-0 z-40 h-full w-[300px] bg-primary-100 bg-opacity-90 drop-shadow-xl"
+        >
+          <motion.div
+             initial="closed"
+             animate={isMenuToggled ? "open" : "closed"}
+             variants={variants}
+             transition={{ duration: 1 }}
+             className="flex justify-end p-12">
+            <button
+              className="rounded-full bg-secondary-500 p-2"
+              onClick={() => setIsMenuToggled(isMenuToggled => !isMenuToggled)}
+            >
+              <XCircleIcon className="h-6 w-6 text-gray-400" />
+            </button>
+          </motion.div>
 
-                    {/* MOBILE MENU LINKS */}
-                    <div className="flex flex-col ml-[33%] gap-10 text-2xl">
-                        <Link 
-                            page="Home"
-                            selectedPage={selectedPage}
-                            setSelectedPage={setSelectedPage}
-                        />
-                        <Link 
-                            page="Benefits"
-                            selectedPage={selectedPage}
-                            setSelectedPage={setSelectedPage}
-                        />
-                        <Link 
-                            page="Our Classes"
-                            selectedPage={selectedPage}
-                            setSelectedPage={setSelectedPage}
-                        />
-                        <Link 
-                            page="Contact us"
-                            selectedPage={selectedPage}
-                            setSelectedPage={setSelectedPage}
-                        />
-                    </div>
-                </div>
-            )
-        }
+          {/* MOBILE MENU LINKS */}
+          <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Benefits"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Our Classes"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contact us"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
+        </motion.div>
+      )}
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
